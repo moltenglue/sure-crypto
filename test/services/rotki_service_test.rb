7012e8fc3c838e0935db50c7df21aaf7c12e82e4
Developer <developer@sure-crypto.local>
2026-02-23 21:02:37 -0500

    Add missing Rotki service tests for logout, blockchain_balances, exchange_balances, manual_balances, and periodic_data

--- test/services/rotki_service_test.rb
+++ test/services/rotki_service_test.rb
@@ -29,6 +29,46 @@ class RotkiServiceTest < ActiveSupport::TestCase
     assert_equal "1.0", result.dig("result", "total", "ETH", "amount")
   end
 
+  test "logs out user from Rotki" do
+    stub_rotki_api(:patch, "/api/1/users/testuser", { "result" => true, "message" => "" })
+    
+    result = @service.logout("testuser")
+    
+    assert_equal true, result["result"]
+  end
+
+  test "fetches blockchain balances" do
+    stub_rotki_api(:get, "/api/1/balances/blockchain", { "result" => { "ETH" => { "amount" => "1.0", "usd_value" => "3000.0" } }, "message" => "" })
+    
+    result = @service.blockchain_balances
+    
+    assert_equal "1.0", result["result"]["ETH"]["amount"]
+  end
+
+  test "fetches exchange balances" do
+    stub_rotki_api(:get, "/api/1/balances/exchanges", { "result" => { "binance" => { "total" => { "BTC" => { "amount" => "0.5" } } } }, "message" => "" })
+    
+    result = @service.exchange_balances
+    
+    assert_equal "0.5", result["result"]["binance"]["total"]["BTC"]["amount"]
+  end
+
+  test "fetches manual balances" do
+    stub_rotki_api(:get, "/api/1/balances/manual", { "result" => [{ "label" => "My Wallet", "amount" => "1000", "currency" => "USD" }], "message" => "" })
+    
+    result = @service.manual_balances
+    
+    assert_equal 1, result["result"].size
+  end
+
+  test "fetches periodic data" do
+    stub_rotki_api(:get, "/api/1/periodic", { "result" => { "last_balance_save" => 1234567890 }, "message" => "" })
+    
+    result = @service.periodic_data
+    
+    assert_equal 1234567890, result["result"]["last_balance_save"]
+  end
+
   private
 
   def stub_rotki_api(method, path, response)
